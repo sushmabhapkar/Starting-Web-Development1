66730c6948372886a88bf23ce09c3d34f0642793
sushmabhapkar <sushmabhapkar244@gmail.com>
2023-10-04 22:31:00 +0530

    task07

--- dom.js
+++ dom.js
@@ -57,30 +57,103 @@ header.style.borderBottom='solid 3px #000'
 //===========================================TASK 06==========================================================
 //****************************QUERYSELECTOR*************************************
 //1.Make the 2nd item have green background color
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor='red';
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='red';
 
 //Make the 3rd item invisible
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display='none';
+// var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display='none';
 //------------------------------------------------------------------------------------------------
 //****************************************2.querySelectorAll*************************************
 //1.Using QuerySelectorALL change the font color to green for 2nd item in the item list
 
- var listItems = document.querySelectorAll('li');
+//  var listItems = document.querySelectorAll('li');
 
-    console.log(listItems)
-   listItems[1].style.color = 'green';
+//     console.log(listItems)
+//    listItems[1].style.color = 'green';
 
 
 //2.Choose all the odd elements and make their background green using QuerySelectorALL﻿
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='#f4f4f4';
-}
-
-
-
-
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor='#f4f4f4';
+// }
 //============================================================================================================
+//*******************************************TASK NO=07***************************************************88 */
+//=============================================Traversing the DOM=========================================================
+var itemList=document.querySelector('#items');
+//************1.parentNode************ */
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+//=======================================================================================================================
+//***************2.parentElement*******************************/
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor='#ccc';
+// console.log(itemList.parentElement.parentElement.parentElement);
+//=================================================================================================================================
+//****************3.childNode**************************************** */
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor='yellow';
+
+//*******************4.firstChild************************************************ */
+//console.log(itemList.firstChild);
+//********************5.firstElementChlid********************** */
+
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Sushma';
+
+//************************6.lastElementChild*********************** */
+
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Onkar';
+
+//**********************7.nextSibling************************ */
+// console.log(itemList.nextSibling);
+
+//***********************8.nextElementSibling***************************** */
+
+//console.log(itemList.nextElementSibling);
+
+//***********************9.previousSibling************************************ */
+
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='red';
+
+
+//************************10.createElement************************************** */
+//create a div
+
+var newdiv=document.createElement('div');
+
+//Add class
+
+newdiv.className='Adwika';
+
+//add id
+
+newdiv.id='princess';
+
+
+
+//add attributes
+
+newdiv.setAttribute('title','my baby');
+
+
+//create text node
+
+var newdivtext=document.createTextNode('hello Everyone');
+//add text to the div
+newdiv.appendChild(newdivtext);
+
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+
+console.log(newdiv);
+
+container.insertBefore(newdiv,h1);
