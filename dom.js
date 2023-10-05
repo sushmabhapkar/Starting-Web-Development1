1a071c5728d02dbe56719e8b893eea15d22f0ae4
sushmabhapkar <sushmabhapkar244@gmail.com>
2023-10-05 14:53:31 +0530

    TASK08

--- dom.js
+++ dom.js
@@ -126,34 +126,97 @@ var itemList=document.querySelector('#items');
 //************************10.createElement************************************** */
 //create a div
 
-var newdiv=document.createElement('div');
+// var newdiv=document.createElement('div');
 
-//Add class
+// //Add class
 
-newdiv.className='Adwika';
+// newdiv.className='Adwika';
 
-//add id
+// //add id
 
-newdiv.id='princess';
-
-
-
-//add attributes
-
-newdiv.setAttribute('title','my baby');
+// newdiv.id='princess';
 
 
-//create text node
 
-var newdivtext=document.createTextNode('hello Everyone');
-//add text to the div
-newdiv.appendChild(newdivtext);
+// //add attributes
 
-
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
+// newdiv.setAttribute('title','my baby');
 
 
-console.log(newdiv);
+// //create text node
 
-container.insertBefore(newdiv,h1);
+// var newdivtext=document.createTextNode('hello Everyone');
+// //add text to the div
+// newdiv.appendChild(newdivtext);
+
+
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+
+
+// console.log(newdiv);
+
+// container.insertBefore(newdiv,h1);
+
+
+//=======================================================================Task08========================================================
+//********************1 add Button********************** */
+var form=document.getElementById('addForm');
+var itemList=document.getElementById('items');
+
+//Form Submit Event
+form.addEventListener('submit',addItem);
+//******************Delete event listner */
+
+itemList.addEventListener('click',removeItem);
+//add item
+function addItem(e)
+{
+
+    e.preventDefault();
+  
+    //get input value
+    
+    var newItem=document.getElementById('item').value;
+
+
+    //create new li element
+    var li=document.createElement('li');
+
+     //Add class
+    li.className='list-group-item';
+
+    //Add text node with input value;
+    li.appendChild(document.createTextNode(newItem));
+    //create delete button
+
+    var deleteBtn=document.createElement('button');
+
+    //Add class to the button
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+
+    //append text node
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+        
+    //append btn to li
+
+    li.appendChild(deleteBtn);
+    //append li to list
+    itemList.appendChild(li);
+
+}
+  //***********remove item */
+  function removeItem(e)
+  {
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are you sure?'))
+        {
+            var li=e.target.parentElement;
+            itemList.removeChild(li);
+        }
+     }
+    
+  }
