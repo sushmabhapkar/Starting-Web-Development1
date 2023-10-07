8b024ea769370360fa1226eb459b3b9a3ea456f3
sushmabhapkar <sushmabhapkar244@gmail.com>
2023-10-07 21:27:03 +0530

    TASK10

---
+++ js_for-bignners/strorage.js
@@ -0,0 +1,30 @@
+// localStorage.setItem('name','Bob')
+// console.log(localStorage.getItem('names'))
+
+
+// sessionStorage.setItem('name','sushma');
+// console.log(sessionStorage.getItem('name'))
+// console.log(typeof getItem)
+// sessionStorage.removeItem('name');
+
+// document.cookie='name=sushma'
+
+var form=document.getElementById('userForm');
+form.addEventListener('submit',addItem);
+
+function addItem(e)
+{
+    e.preventDefault();
+
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+
+    const userDetails={
+        name:name,
+        email:email
+    };
+    localStorage.setItem('userDetails',JSON.stringify(userDetails));
+    alert("Details have been stored in local storage.");
+
+};
+
