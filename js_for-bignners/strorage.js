31b2c0102bfe9ac08bf3e32962d714c6b1908a1a
sushmabhapkar <sushmabhapkar244@gmail.com>
2023-10-29 21:37:31 +0530

    TASK13

--- js_for-bignners/strorage.js
+++ js_for-bignners/strorage.js
@@ -9,44 +9,52 @@
 
 // document.cookie='name=sushma'
 
-var form=document.getElementById('userForm');
-const list=document.getElementById('list');
-console.log(list)
-form.addEventListener('submit',addItem);
 
-function addItem(e)
-{
+var form = document.getElementById('userForm');
+const list = document.getElementById('list');
+console.log(list);
+form.addEventListener('submit', addItem);
+
+function addItem(e) {
     e.preventDefault();
 
-    const name=document.getElementById('name').value;
-    const email=document.getElementById('email').value;
-    const phonenumber=document.getElementById('phonenumber').value;
-    // Retrieve the existing user details from local storage
-    const storedUserDetails = JSON.parse(localStorage.getItem('userDetails')) || [];
-
-    //generate unique key
- //const uniqueKey=Date.now();
-
-    const userDetails={
-        name:name,
-        email:email,
-        phonenumber:phonenumber
-
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phonenumber = document.getElementById('phonenumber').value;
+    
+    const userDetails = {
+        name: name,
+        email: email,
+        phonenumber: phonenumber
     };
 
-    localStorage.setItem(userDetails.email,JSON.stringify(userDetails));
+    localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
     showUserOnScreen(userDetails);
 
-
-    //Add the new user details to the existing array
-       function showUserOnScreen(userDetails)
+    // Add a new user details to the list with a delete button
+    function showUserOnScreen(userDetails) {
+        const parentElem = document.getElementById('list');
+        const childElem = document.createElement('li');
+        childElem.textContent = userDetails.name + '-' + userDetails.email + '-' + userDetails.phonenumber;
+       
+       
+       const deleteButton=document.createElement('input');
+       deleteButton.type="button";
+       deleteButton.value='delete';
+       deleteButton.onclick=()=>
        {
-                  const parentElem=document.getElementById('list');
-                  const childElem=document.createElement('li');
-                  childElem.textContent=userDetails.name+'-'+userDetails.email+'-'+userDetails.phonenumber;
-                  parentElem.appendChild(childElem);
-            
+        localStorage.removeItem(userDetails.email);
+        parentElem.removeChild(childElem);
        }
+       
+       childElem.appendChild(deleteButton);
+        parentElem.appendChild(childElem);
+
+        
+    }
+
+
+
 
 
 
