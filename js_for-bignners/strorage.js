549a01430d3663b96120efd49d835eb7e3b0ea16
sushmabhapkar <sushmabhapkar244@gmail.com>
2023-10-29 14:32:11 +0530

    Task12

--- js_for-bignners/strorage.js
+++ js_for-bignners/strorage.js
@@ -10,6 +10,8 @@
 // document.cookie='name=sushma'
 
 var form=document.getElementById('userForm');
+const list=document.getElementById('list');
+console.log(list)
 form.addEventListener('submit',addItem);
 
 function addItem(e)
@@ -18,15 +20,53 @@ function addItem(e)
 
     const name=document.getElementById('name').value;
     const email=document.getElementById('email').value;
-//generate unique key
- const uniqueKey=Date.now();
+    const phonenumber=document.getElementById('phonenumber').value;
+    // Retrieve the existing user details from local storage
+    const storedUserDetails = JSON.parse(localStorage.getItem('userDetails')) || [];
+
+    //generate unique key
+ //const uniqueKey=Date.now();
 
     const userDetails={
         name:name,
-        email:email
+        email:email,
+        phonenumber:phonenumber
+
     };
-    localStorage.setItem('userDetails_${uniqueKey}',JSON.stringify(userDetails));
-    alert("Details have been stored in local storage.");
+
+    localStorage.setItem(userDetails.email,JSON.stringify(userDetails));
+    showUserOnScreen(userDetails);
+
+
+    //Add the new user details to the existing array
+       function showUserOnScreen(userDetails)
+       {
+                  const parentElem=document.getElementById('list');
+                  const childElem=document.createElement('li');
+                  childElem.textContent=userDetails.name+'-'+userDetails.email+'-'+userDetails.phonenumber;
+                  parentElem.appendChild(childElem);
+            
+       }
+
+
+
+
+
+   
+    // localStorage.setItem('userDetails', JSON.stringify(storedUserDetails))||[];
+    // alert("Details have been stored in local storage.");
+    // const li= document.createElement('li')
+    // const nameElement = document.createElement('span');
+    // nameElement.textContent = `Name: ${name}`;
+    // const emailElement = document.createElement('span');
+    // emailElement.textContent = `Email: ${email}`;
+    // const phoneElement = document.createElement('span');
+    // phoneElement.textContent = `Phone Number: ${phonenumber}`;
+    // li.appendChild(nameElement)
+    // li.appendChild(emailElement)
+    // li.appendChild(phoneElement)
+    // list.appendChild(li);
+      
 
 };
 
